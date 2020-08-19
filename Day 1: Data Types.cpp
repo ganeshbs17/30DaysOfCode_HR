1cf139bcf0b77298cd6cc1c29245f721a51166f5
Ganesh Savant <65601315+ganeshbs17@users.noreply.github.com>
2020-08-19 12:53:59 +0530

    Create Day 1: Data Types.cpp

---
+++ Day 1: Data Types.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+    // Declare second integer, double, and String variables.
+    int i1;
+    double d1,d2;
+    string s1;
+    // Read and save an integer, double, and String to your variables.
+    cin>>i1>>d1;
+    getline(cin,s1);
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+
+d2=d+d1;
+
+    cout<<i+i1<<"\n";
+    cout<<d2<<"\n";
+    cout<<s<<s1;
+    // Print th sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+    return 0;
