70ed95ffe546b297c4012fdac0c4415c4e6e69c3
Ganesh Savant <65601315+ganeshbs17@users.noreply.github.com>
2020-08-22 15:49:27 +0530

    Create Day 6: Let's Review.cpp

---
+++ Day 6: Let's Review.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        string s,s1,s2;
+        cin>>s;
+        for(int j=0;j<s.size();j++)
+        {
+           if(j%2==0)
+             s1+=s[j];
+           else
+             s2+=s[j];
+        }
+        cout<<s1<<" "<<s2<<endl;
+    } 
+    return 0;
+}
